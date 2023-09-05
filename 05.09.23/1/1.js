c46ca9bbd8f9dcb686381db96fac882a509c1367
Anastasia1ukina <lukina02anastasiy@gmail.com>
2023-09-05 21:29:47 +0300

    task 1 forEach

--- 05.09.23/1/1.js
+++ 05.09.23/1/1.js
@@ -1,5 +1,5 @@
 function isDivisible(){
-    for (let i =0; i<arguments.length; i++) {
+    for (let i = 0; i<arguments.length; i++) {
       if(arguments[0] % arguments[i] !== 0) {
         return false;
       }
@@ -8,3 +8,17 @@ function isDivisible(){
   }
   
   console.log(isDivisible(24, 4))
+
+  //метод forEach
+
+//   function isDivisible(...arguments){
+//     let flag = true;
+//     arguments.forEach((argument) => {
+//       if(arguments[0] % argument !== 0) {
+//         flag = false;
+//       }
+//     })
+//       return flag;
+//   }
+  
+//   console.log(isDivisible(3, 3, 4))

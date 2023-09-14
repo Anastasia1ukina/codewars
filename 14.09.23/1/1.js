005aebc54532167848916dbd849b41ca71a02ad2
Anastasia1ukina <lukina02anastasiy@gmail.com>
2023-09-15 00:07:49 +0300

    task 1

--- 14.09.23/1/1.js
+++ 14.09.23/1/1.js
@@ -1,3 +1,12 @@
-String.prototype.replaceAt = function(index, replacement) {
-    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
-  }
+function maskify(cc) {
+    return cc.replace(/.(?=.{4})/g, "#");
+}
+
+function maskify(cc) {
+    if (cc.length < 4) {
+        return cc;
+    } else {
+        let str = cc.slice(0, cc.length - 4)
+        return str.replace(/[^#]/g, '#') + cc.slice(cc.length - 4, cc.length + 1)
+    }
+}

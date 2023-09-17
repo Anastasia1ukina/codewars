90878abf246a4e2198365c41c2b8d190f296a49b
Anastasia1ukina <lukina02anastasiy@gmail.com>
2023-09-17 20:48:06 +0300

    task 1

---
+++ 17.09.23/1/1.js
@@ -0,0 +1,13 @@
+function longest(s1, s2) {
+    let s1Arr = s1.split('')
+    let s2Arr = s2.split('')
+
+    let allStr = [...s1Arr, ...s2Arr]
+    allStr.sort()
+
+    let uniqueChars = allStr.filter((element, index) => {
+        return allStr.indexOf(element) === index
+    })
+
+    return uniqueChars.join('')
+}

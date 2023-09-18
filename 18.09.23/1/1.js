c4ddc2bfbbf91722d3274fbd0aca97660f2eea19
Anastasia1ukina <lukina02anastasiy@gmail.com>
2023-09-18 23:53:01 +0300

    task 1

---
+++ 18.09.23/1/1.js
@@ -0,0 +1,10 @@
+function printerError(s) {
+    let str = ''
+    let strLength = s.length
+      if (s.match(/[a-m]/gi)) {
+          str = `0/${s.length}`
+          return str 
+       } else {
+         return s.every(n => )
+       }
+  }

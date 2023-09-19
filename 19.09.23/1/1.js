80b4f832b743b9cc9a1709b89ef04ff01f6e3576
Anastasia1ukina <lukina02anastasiy@gmail.com>
2023-09-19 23:37:52 +0300

    task 1

--- 19.09.23/1/1.js
+++ 19.09.23/1/1.js
@@ -5,3 +5,5 @@ function validatePIN(pin) {
     let pinDigit = pin.match(/[0-9]/g)
     return (pinDigit.length === 6 && pin.length === 6) || (pinDigit.length === 4 && pin.length === 4)
 }
+
+const validatePIN = pin => /^(\d{4}$|\d{6}$)/.test(pin);

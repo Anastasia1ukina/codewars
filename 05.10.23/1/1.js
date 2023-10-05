305a7f43070187b54ed6e210f456e8ecfe18bf93
Anastasia1ukina <lukina02anastasiy@gmail.com>
2023-10-05 14:10:10 +0300

    task 1

--- 05.10.23/1/1.js
+++ 05.10.23/1/1.js
@@ -12,3 +12,9 @@ function bouncingBall(h, bounce, window) {
     }
     return count - 1;
 }
+
+function bouncingBall(h, bounce, window) {
+    var rebounds = -1;
+    if (bounce > 0 && bounce < 1) while (h > window) rebounds += 2, h *= bounce;
+    return rebounds;
+}

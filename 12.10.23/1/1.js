2aa5fb94892552f660f177d4fe8e545ce376bbc5
Anastasia1ukina <lukina02anastasiy@gmail.com>
2023-10-13 01:12:26 +0300

    ver2

--- 12.10.23/1/1.js
+++ 12.10.23/1/1.js
@@ -1,1 +1,10 @@
 const stray = nums => nums.reduce((a, b) => a ^ b);
+
+function stray(numbers) { // [1, 1, 2]
+    let two = new Set(numbers) // {1,2}
+    let nums = Array.from(two) // nums[0] = 1
+    if (numbers.filter(x => x==nums[0]).length == 1) { // [1,1].length = 2
+      return nums[0]                                   // [2].length = 1
+    }
+    return nums[1]
+  }

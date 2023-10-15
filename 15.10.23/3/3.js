32270973b58de47a7e71a0b8d9d873af658abbc1
Anastasia1ukina <lukina02anastasiy@gmail.com>
2023-10-15 23:45:26 +0300

    task3

--- 15.10.23/3/3.js
+++ 15.10.23/3/3.js
@@ -13,6 +13,8 @@ function comp(array1, array2) {
 
 function comp(array1, array2) {
     if (array1 == null || array2 == null) return false;
-    array1.sort((a, b) => a - b); array2.sort((a, b) => a - b);
-    return array1.map(v => v * v).every((v, i) => v == array2[i]);
+    array2.sort((a, b) => a - b);
+    return array1.map(v => v * v).sort((a, b) => a - b).every((v, i) => v == array2[i]);
 }
+
+console.log(comp([2, 3], [4, 4]))

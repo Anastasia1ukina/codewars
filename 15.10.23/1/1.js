1bb4f2d630a869f9da9446987f4701042353ce0e
Anastasia1ukina <lukina02anastasiy@gmail.com>
2023-10-15 15:40:48 +0300

    task 1, 2

--- 15.10.23/1/1.js
+++ 15.10.23/1/1.js
@@ -1,10 +1,19 @@
+// function twoSum(numbers, target) {
+//     for (let i = 0; i < numbers.length; i++) {
+//         let diff = target - numbers[i];
+//         let second = numbers.slice(i + 1).indexOf(diff);
+//         if (second !== -1) {
+//             console.log(i, second + i + 1)
+//             return [i, second + i + 1]
+//         }
+//     }
+// }
+
 function twoSum(numbers, target) {
     for (let i = 0; i < numbers.length; i++) {
-        let diff = target - numbers[i];
-        let second = numbers.slice(i + 1).indexOf(diff);
-        if (second !== -1) {
-            console.log(i, second + i + 1)
-            return [i, second + i + 1]
+        let second = numbers.indexOf(target - numbers[i], i + 1);
+        if (second > i) {
+            return [i, second];
         }
     }
 }

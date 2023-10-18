34d62ef037be0e9ebc899f34e247133cb8a82409
Anastasia1ukina <lukina02anastasiy@gmail.com>
2023-10-18 10:12:41 +0300

    task 1

--- 16.10.23/1/1.js
+++ 16.10.23/1/1.js
@@ -1,13 +1,15 @@
 function reverse(str) {
+    if (str.trim() === '') {
+        return '';
+    }
+
     let res = [];
     let strArr = str.split(' ')
-    for (let i = 1; i <= strArr.length; i += 2) {
+    for (let i = 1; i < strArr.length; i += 2) {
         res = strArr[i].split('').reverse().join('')
-        let k = strArr.splice(i, i, res);
-        console.log(i)
+        let k = strArr.splice(i, 1, res);
     }
-    res = strArr.join(' ')
-    console.log(res)
-}
 
-console.log(reverse("I really don't like reversing strings!"))
+    res = strArr.join(' ')
+    return res;
+}

f8d8f25a112e3d9067f70e4073c9b4873b74ff6d
Anastasia1ukina <lukina02anastasiy@gmail.com>
2023-10-20 17:41:28 +0300

    task 2

---
+++ leetcode/20.10.23/Longest Common Prefix/1.js
@@ -0,0 +1,18 @@
+var longestCommonPrefix = function (strs) {
+    strs.sort()
+    let first = strs[0]
+    let last = strs[strs.length - 1]
+    let ans = ''
+
+    if (strs.length === 1) return strs[0]
+
+    for (let i = 0; i < first.length; i++) {
+        if (first[i] !== last[i]) {
+            return ans
+        } else {
+            ans += first[i];
+        }
+
+    }
+    return ans
+};
